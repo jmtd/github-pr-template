fb6a2fe608d8ae6721e4b9fee47f77070ff37fd4
Jonathan Dowland <jdowland@redhat.com>
2017-08-21 12:00:07 +0100

    sync-all.sh: only run for OpenShift images
    Determined by presence of "openshift" in the repository name.
    
    I've left the non-openshift submodules in for now, as we plan to add
    a standalone-specific pull template.

--- sync-all.sh
+++ sync-all.sh
@@ -3,4 +3,7 @@ set -u
 set -e
 
 git submodule update --remote --force
-git submodule foreach "../../sync.sh"
+git submodule foreach --quiet '
+    if echo "$name" | grep -q openshift; then
+        ../../sync.sh;
+    fi'
