6b61a03ed8b5e4ceca11bc8628061e012a9983bc
Jonathan Dowland <jdowland@redhat.com>
2017-08-31 09:44:41 +0100

    Small improvements to init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ set -e
 
 # Users must run this after a fresh clone of this repository
 
-git submodule update --init
-git submodule foreach "
-    if [ \"\$(git remote | wc -l)\" -lt 2 ]; then
+git submodule update --init --remote --force
+git submodule foreach '
+    if [ "$(git remote | wc -l)" -lt 2 ]; then
         hub fork;
-    fi"
+    fi'
