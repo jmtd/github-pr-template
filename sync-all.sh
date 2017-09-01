fb4085c50bb1f7588fa58059bba54c8894f07a91
Jonathan Dowland <jdowland@redhat.com>
2017-09-01 15:30:01 +0100

    sync-all.sh: require interactive confirmation
    require an interactive confirmation to run the script, to avoid people
    accidentally running it and raising a load of PRs.

--- sync-all.sh
+++ sync-all.sh
@@ -2,5 +2,10 @@
 set -u
 set -e
 
+echo WARNING: this script will potentially generate a LOT of Pull Requests across
+echo a lot of repositories. Make sure that is REALLY what you want to do. Hit enter
+echo to continue, otherwise kill this script now.
+read confirm
+
 git submodule update --remote --force --quiet
 git submodule foreach --quiet ../../sync.sh
